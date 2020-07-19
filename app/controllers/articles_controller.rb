e58e83a4fafcb63a6e594971847f7b1b3274937c
fanny <fanny@guava.com.br>
2020-07-19 13:17:20 -0300

    Adds delete action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,6 +37,13 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
 
   # public methods (index, show, new, edit, create, update and destroy) placed before private methods
 
