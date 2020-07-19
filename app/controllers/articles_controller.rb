643a05fb794a7b883396541ef84176c57fda56ff
fanny <fanny@guava.com.br>
2020-07-19 01:31:05 -0300

    Creates an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
   def new
   end
+
+  def create
+    render plain: params[:article].inspect
+    # displaying the parameters for the article that are coming in from the form
+  end
 end
