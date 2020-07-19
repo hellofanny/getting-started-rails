3af3cd408ca941173f7e4cc1e11123491a8935bd
fanny <fanny@guava.com.br>
2020-07-19 12:59:19 -0300

    Adds some validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,12 +2,13 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
@@ -15,8 +16,11 @@ class ArticlesController < ApplicationController
     # displaying the parameters for the article that are coming in from the form
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
 
