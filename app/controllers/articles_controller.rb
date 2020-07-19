726fd4ede4f069014161e994a4c931106336d95b
fanny <fanny@guava.com.br>
2020-07-19 01:42:19 -0300

    Saves data in the controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,16 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
     # displaying the parameters for the article that are coming in from the form
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
