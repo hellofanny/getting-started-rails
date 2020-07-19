b947ce900eb3aaec40ad7a876a60d9590ea87ea8
fanny <fanny@guava.com.br>
2020-07-19 01:09:39 -0300

    Creates a new resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  resources :articles
+  #method which can be used to declare a standard REST resource
+
   root 'welcome#index'
   # tells Rails to map requests to the root of the application to the welcome controller's index action and get 'welcome/index' tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action.
 end
