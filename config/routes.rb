92249c48c61286178b69d80323e592c93e693f11
fanny <fanny@guava.com.br>
2020-07-18 23:14:48 -0300

    Set application home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
+  # tells Rails to map requests to the root of the application to the welcome controller's index action and get 'welcome/index' tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action.
 end
